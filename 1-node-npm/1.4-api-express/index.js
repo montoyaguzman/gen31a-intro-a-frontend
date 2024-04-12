aa1bec49b7ab56814c8054af225f49570032bd1c
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-04-11 19:51:34 -0600

    feat: proyecto api en npm

---
+++ 1-node-npm/1.4-api-express/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// songs 
+const songs = [
+    { id: 1, name: 'Kilometros', artist: 'Caligaris', year: 2007 },
+    { id: 2, name: 'Umbrella', artist: 'Rihanna', year: 2000 },
+    { id: 3, name: 'Back in black', artist: 'AC/DC', year: 1980 },
+    { id: 4, name: 'Gasolina', artist: 'Daddy Yankee', year: 2002 },
+];
+
+// url - endpoint getSongs
+app.get('/', (req, res) => {
+    res.json(songs);
+});
+
+app.listen(port, () => {
+    console.log('Servidor de express esta corriendo', port);
+});
