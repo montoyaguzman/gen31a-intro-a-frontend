ef3e3282e6e43b82f24a12157905caacd52e64e9
montoyaguzman7 <montoyaguzman7@gmail.com>
2024-04-23 21:06:56 -0600

    feat: estilos globales y avance maquetado Menu

--- 3-trainers-project/trainers-pokemon-frontend/src/components/Menu/index.jsx
+++ 3-trainers-project/trainers-pokemon-frontend/src/components/Menu/index.jsx
@@ -2,7 +2,22 @@ import './index.css';
 
 function Menu() {
     return (
-        <h1>Menu component works!</h1>
+        <nav>
+            <div>
+                <img src="" alt="logo de trainers" className='logo-img' />
+            </div>
+            <ul className='nav-items'>
+                <a href="/" className='nav-link'>
+                    Inicio
+                </a>
+                <a href="/new-trainer" className='nav-link'>
+                    Registro entrenador
+                </a>
+                <a href="/about" className='nav-link'>
+                    Acerca de nosotros
+                </a>
+            </ul>
+        </nav>
     )
 }
 
